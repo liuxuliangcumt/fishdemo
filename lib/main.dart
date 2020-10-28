51f3be1e502f1d07043f00873848dc6582a0e011
liuxuliangcumt <1390927050@qq.com>
2020-10-28 16:03:34 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -30,23 +30,444 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey _key1 = GlobalKey();
+  Shader shader;
+  Gradient gradient;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    gradient = LinearGradient(
+        begin: Alignment.centerLeft,
+        end: Alignment.centerRight,
+        colors: [
+          Color.fromARGB(255, 219, 195, 252),
+          Color.fromARGB(255, 148, 197, 252)
+        ]);
+    shader = gradient.createShader(
+        Rect.fromLTWH(size.width / 2 - 80, 0, size.width / 2 + 20, 310));
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-          child: Column(
-        children: [
-          Image.asset(
-            "assets/1.webp",
-          ),
-          Image.asset(
-            "assets/2.gif",
-          ),
-        ],
-      )),
-    );
+        backgroundColor: Colors.black,
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
+                ),
+                child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        "assets/1.webp",
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        '你的Mate 自带光环',
+                        style: TextStyle(
+                            fontSize: 20, foreground: Paint()..shader = shader),
+                      ),
+                      SizedBox(height: 20),
+                      Image.asset(
+                        "assets/2.webp",
+                      ),
+                      Container(
+                        width: size.width,
+                        height: size.width / 1080 * 1773,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          children: [
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/3.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/4.png",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/5.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/6.webp",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                            Image.asset(
+                              "assets/4_1.webp",
+                              width: size.width,
+                            ),
+                            Image.asset(
+                              "assets/4_2.webp",
+                              width: size.width,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 260,
+                        child: Image.asset(
+                          "assets/hua.gif",
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Text(
+                        '你的Mate 芯潮澎湃',
+                        style: TextStyle(
+                            fontSize: 20, foreground: Paint()..shader = shader),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        width: size.width,
+                        height: size.width / 1080 * 1773,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          children: [
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/7.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/8.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/4.png",
+                                  width: size.width,
+                                  fit: BoxFit.cover,
+                                  height: 100,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/7_1.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/4.png",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/7_2.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/4.png",
+                                  width: size.width,
+                                  fit: BoxFit.cover,
+                                  height: 150,
+                                ),
+                              ],
+                            ),
+                            Image.asset(
+                              "assets/8_2.webp",
+                              width: size.width,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 260,
+                        child: Image.asset(
+                          "assets/hua.gif",
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Text(
+                        '你的Mate 光影同行',
+                        style: TextStyle(
+                            fontSize: 20, foreground: Paint()..shader = shader),
+                      ),
+                      SizedBox(height: 20),
+                      Image.asset(
+                        "assets/9.webp",
+                        width: size.width,
+                      ),
+                      Image.asset(
+                        "assets/10.webp",
+                        width: size.width,
+                      ),
+                      Container(
+                        width: size.width,
+                        height: size.width / 1080 * 1181,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          children: [
+                            Image.asset(
+                              "assets/11.webp",
+                              width: size.width,
+                            ),
+                            Image.asset(
+                              "assets/11_1.webp",
+                              width: size.width,
+                            ),
+                            Image.asset(
+                              "assets/11_2.webp",
+                              width: size.width,
+                            ),
+                          ],
+                        ),
+                      ), //光影同行
+                      SizedBox(
+                        width: 260,
+                        child: Image.asset(
+                          "assets/hua.gif",
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Container(
+                        width: size.width,
+                        height: size.width / 1080 * 1850,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          children: [
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/s_1.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/s_2.gif",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/s_3.webp",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/s_4.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/s_5.gif",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/s_6.webp",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/s_7.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/s_8.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/s_9.webp",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/s_11.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/s_12.webp",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 260,
+                        child: Image.asset(
+                          "assets/hua.gif",
+                        ),
+                      ),
+                      //动态摄影
+                      Image.asset(
+                        "assets/d_1.webp",
+                        width: size.width,
+                      ),
+
+                      Container(
+                        width: size.width,
+                        height: size.width / 1080 * 1050,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          children: [
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/d_2.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/d_3.gif",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/d_4.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/d_5.gif",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/d_6.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/d_7.gif",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 260,
+                        child: Image.asset(
+                          "assets/hua.gif",
+                        ),
+                      ),
+                      Image.asset(
+                        "assets/13.webp",
+                        width: size.width,
+                      ),
+                      Image.asset(
+                        "assets/d_3.gif",
+                        width: size.width,
+                      ),
+
+                      SizedBox(height: 40),
+                      Text(
+                        '你的Mate 贴心懂你',
+                        style: TextStyle(
+                            fontSize: 20, foreground: Paint()..shader = shader),
+                      ),
+                      SizedBox(height: 20),
+                      Image.asset(
+                        "assets/x.webp",
+                        width: size.width,
+                      ),
+
+                      Container(
+                        width: size.width,
+                        height: size.width / 1080 * 1050,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          children: [
+                            Image.asset(
+                              "assets/x_1.webp",
+                              width: size.width,
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/x_2.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/x_4.gif",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/x_5.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/x_6.webp",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  "assets/x_7.webp",
+                                  width: size.width,
+                                ),
+                                Image.asset(
+                                  "assets/x_8.webp",
+                                  width: size.width,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 260,
+                        child: Image.asset(
+                          "assets/hua.gif",
+                        ),
+                      ),
+
+                      Image.asset(
+                        "assets/m_1.webp",
+                        width: size.width,
+                      ),
+                      Image.asset(
+                        "assets/m_2.webp",
+                        width: size.width,
+                      ),
+                      Image.asset(
+                        "assets/m_3.webp",
+                        width: size.width,
+                      ),
+                      Image.asset(
+                        "assets/m_4.webp",
+                        width: size.width,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
